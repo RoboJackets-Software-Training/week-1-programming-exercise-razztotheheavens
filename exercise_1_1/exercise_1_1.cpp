2662dd92de29ed27dccedc07a06f3a46bf62346e
rohaiz-haris <your email addresss here>
2020-09-24 21:33:40 +0000

    Completed exercise 1.1

--- exercise_1_1/exercise_1_1.cpp
+++ exercise_1_1/exercise_1_1.cpp
@@ -5,20 +5,17 @@
 int main() {
 	std::cout << "Use the wasd keys and enter to input a direction, or the E key to stop" << std::endl;
 
-    // Write your code for step 1 here
 	std::vector<float>wheel_speeds(4);
-    // Write your code for step 2 here
-   wheel_speeds[0] = 0;
-   wheel_speeds[1] = 0;
-   wheel_speeds[2] = 0;
-   wheel_speeds[3] = 0;
-    // Do not edit the next 5 lines
+    wheel_speeds[0] = 0;
+    wheel_speeds[1] = 0;
+    wheel_speeds[2] = 0;
+    wheel_speeds[3] = 0;
+     // Do not edit the next 5 lines
     bool keep_going = true;
     char direction;
 
     while (keep_going) {
         std::cin >> direction; //At the begining of every iteration look for an input from the user
-        // Write rest of code here (within while loop'
         	if (direction == 'w' || direction == 'a'||direction == 's'||direction=='d'||direction == 'e') {
         		if (direction == 'w') {
         			for (int i = 0; i < 4; i++) {
